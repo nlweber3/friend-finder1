86f6fefb67948cc98326db5b77a65bca9e975176
Nick <nlweber3@gmail.com>
2018-03-05 16:18:15 -0800

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [{
+    name: "Barry Allen",
+    photo: "http://theflashpodcast.com/wp-content/uploads/2014/08/Flash-Barry-Allen.jpg",
+    scores: [
+        4,
+        1,
+        4,
+        1,
+        2,
+        2,
+        1,
+        1,
+        3,
+        1
+    ],
+}];
+
+module.exports = friends;
