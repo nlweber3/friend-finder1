5def8e6c135b330434877d5ccaa812eeff53aa86
Nick <nlweber3@gmail.com>
2018-03-14 23:28:50 -0700

    routing fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,6 @@ app.get("/", function(req, res) {
 });
 
 app.get("/survey", function(req,res) {
-	res.sendFile(path.join(_dirname,"../public/survey.html"));
+	res.sendFile(path.join(__dirname,"/../public/survey.html"));
 });
 };
